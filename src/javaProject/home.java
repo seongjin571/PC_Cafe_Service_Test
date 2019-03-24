e69aced6f001fe1c2c9a14e47f5e988000d302e7
16010948 <bjyoung0@gmail.com>
2019-03-24 17:54:17 +0900

    test

--- src/javaProject/home.java
+++ src/javaProject/home.java
@@ -3,7 +3,7 @@ package javaProject;
 public class home {
 
 	public static void main(String[] args) {
-		System.out.println("I want to go home.");
+		System.out.println("I want to go home!");
 
 	}
 
