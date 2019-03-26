528d989f7dbd154b41db1d29abd568e355faba20
ssun-aa <ssk6200@daum.net>
2019-03-26 16:10:02 +0900

    USER GUI

--- src/javaProject/GUI.java
+++ src/javaProject/GUI.java
@@ -1,5 +1,137 @@
 package javaProject;
 
-public class GUI {
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.File;
+import java.io.IOException;
 
+import javax.swing.border.*;
+
+
+public class GUI extends JFrame{
+	JLabel lb1, la1, la2, la3, la4, status;
+	JTextField id, pw;
+	JPanel idPanel, paPanel, loginPanel;
+	JButton b1, b2;
+	JTextArea content;
+	Image img = null;
+	public GUI() {
+		super("Login"); //창 이름 Login
+		setLayout(null); //레이아웃 내가 원하는 위치로
+
+		picture();
+		
+		idpw();
+		
+		Button();
+		
+		setSize(650,900);//전체 창 크기 설정
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);//x눌렀을 때 종료
+		setResizable(false);//창 크기 고정
+	}
+	
+	void picture() {
+		
+		try {
+			File sourceimage = new File("C:\\Users\\이선아\\Desktop\\sejong.png");
+			img = ImageIO.read(sourceimage);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}//그림파일 가져오기
+			
+		la4 = new JLabel(new ImageIcon(img));
+		la4.setBounds(35, 10, 550, 550);
+		add(la4);// 그림 삽입
+		
+	}
+	void idpw(){
+		
+		idPanel = new JPanel();
+		paPanel = new JPanel();
+		la3 = new JLabel("  ID ");
+		la3.setFont(new Font("",Font.BOLD, 26));
+		la2 = new JLabel("PW ");
+		la2.setFont(new Font("",Font.BOLD, 26));//ID, PW 글자 설정
+
+		id = new JTextField(13);
+		id.setFont(new Font("",Font.BOLD, 20));
+		pw = new JPasswordField(13);//입력시 ●로 입력됨
+		pw.setFont(new Font("",Font.BOLD, 20));//id, pw 입력 창 글자 설정
+		
+		idPanel.add(la3);
+		idPanel.add(id);
+		idPanel.setBounds(80, 620, 300, 80);
+		add(idPanel);// ID글자와 박스 삽입
+
+		paPanel.add(la2);
+		paPanel.add(pw);
+		paPanel.setBounds(80, 690, 300, 60);
+		add(paPanel);// PW글자와 박스 삽입
+
+		
+	}
+	
+	void Button() {
+		
+		b1 = new JButton("LOGIN");
+		b2 = new JButton("JOIN");//LOGIN, JOIN 박스 생성
+		
+		b1.setBounds(430,615,100,125);
+		b2.setBounds(250,770,120,40);
+		add(b1);
+		add(b2);//LOGIN, JOIN 박스 설정 후 삽입
+		
+		status = new JLabel();// id+pw 보여줄 label;
+		b1.addActionListener(new ActionListener() {//login버튼 클릭시 발생하는 액션
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String u_id = id.getText();//입력 된 id 가져오기
+				String u_pw = pw.getText();//입력 된 pw 가져오기
+				status.setText(u_id+" "+u_pw);// 확인용; id+pw로 status 설정
+			}
+		});
+		
+		status.setBounds(300, 820, 150, 40);
+		add(status);// id+pw 인 status 설정 후 삽입
+		
+		b2.addActionListener(new ActionListener() {//join버튼 클릭시 발생하는 액션
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Object obj = e.getSource();
+				
+				if((JButton) obj == b2) {// join버튼이 눌리면
+					Join j2 = new Join("Join");// 새 join 생성
+					j2.setVisible(true);
+					j2.setSize(550, 620);//크기 설정
+					j2.setLocation(500, 100);//생성될 위치 설정
+					j2.addWindowListener(new WindowAdapter() {//x누르면 새창만 종료되게
+						public void windowClosing(WindowEvent e) {
+							j2.setVisible(false);
+							j2.dispose();
+						}
+					});
+				}
+			}
+		});
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		GUI j1 = new GUI();
+		
+		
+	}
+	
 }
--- src/javaProject/Join.java
+++ src/javaProject/Join.java
@@ -1,5 +1,117 @@
 package javaProject;
 
-public class Join {
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class Join extends JFrame   {
+	JLabel lb1, la1, la2, la3, la4, status;
+	JTextField id, pw, pw2, email;
+	JPanel idPanel, paPanel, pa2Panel, emPanel;
+	JButton b1, b2;
+	ActionEvent e;
+	
+	Join(String str){
+		super(str);
+		setLayout(null); //레이아웃 내가 원하는 위치로
+		
+		J_idpw();
+		J_button();
+	}
+	
+	
+	
+	void J_idpw() {
+		
+		idPanel = new JPanel();
+		paPanel = new JPanel();
+		pa2Panel = new JPanel();
+		emPanel = new JPanel();
+
+		la1 = new JLabel("ID     ");
+		la1.setFont(new Font("",Font.BOLD, 20));
+		la2 = new JLabel("PW    ");
+		la2.setFont(new Font("",Font.BOLD, 20));
+		la3 = new JLabel("PW2  ");
+		la3.setFont(new Font("",Font.BOLD, 20));
+		la4 = new JLabel("E-mail");
+		la4.setFont(new Font("",Font.BOLD, 20));//ID, PW 등 글자 설정
+		
+		
+		id = new JTextField(13);
+		id.setFont(new Font("",Font.BOLD, 20));
+		pw = new JPasswordField(13);//입력시 ●로 입력됨
+		pw.setFont(new Font("",Font.BOLD, 20));
+		pw2 = new JPasswordField(13);//입력시 ●로 입력됨
+		pw2.setFont(new Font("",Font.BOLD, 20));
+		email = new JTextField(13);
+		email.setFont(new Font("",Font.BOLD, 20));//id, pw 등 입력 창 글자 설정
+		
+		
+		idPanel.add(la1);
+		idPanel.add(id);
+		idPanel.setBounds(80, 140, 300, 60);
+		add(idPanel);// ID글자와 박스 삽입
+
+		paPanel.add(la2);
+		paPanel.add(pw);
+		paPanel.setBounds(80, 220, 300, 60);
+		add(paPanel);// PW글자와 박스 삽입
+
+		pa2Panel.add(la3);
+		pa2Panel.add(pw2);
+		pa2Panel.setBounds(80, 300, 300, 60);
+		add(pa2Panel);// pw2글자와 박스 삽입
+		
+		emPanel.add(la4);
+		emPanel.add(email);
+		emPanel.setBounds(80, 380, 300, 60);
+		add(emPanel);// email글자와 박스 삽입
+	}
+	
+	void J_button() {
+		
+		b1 = new JButton("중복");
+		b2 = new JButton("JOIN");//중복, JOIN 박스 생성
+		
+		b1.setBounds(400,143,100,33);//위치, 크기 설정
+		b1.setBackground(new Color(210,50,50));//색상 빨간색
+		b1.setFont(new Font("",Font.PLAIN,17));//글씨체 설정
+		b1.setForeground(new Color(255,255,255));//글씨 하얀색
+		
+		b2.setBounds(210,480,120,40);
+		b2.setBackground(new Color(210,50,50));
+		b2.setFont(new Font("",Font.PLAIN,17));
+		b2.setForeground(new Color(255,255,255));
+		
+		add(b1);
+		add(b2);//LOGIN, JOIN 박스 설정 후 삽입
+		
+		status = new JLabel();
+		b2.addActionListener(new ActionListener() {//login버튼 클릭시 발생하는 액션
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(!(pw.getText().equals(pw2.getText()))) {//pw의 값과 pw2의 값이 같지 않으면
+					status.setText("비밀번호가 일치하지 않습니다.");
+				}
+			}
+		});
+		
+
+		status.setBounds(190,520,200,40);
+		add(status);// id+pw 인 status 설정 후 삽입
+	}
+
+	
 }
